d67488f53c65c64f4349efa138afb69865b5ddfb
locusfox <lucaswfox7@gmail.com>
2021-11-23 21:08:30 -0600

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Trinary Example
---
+++ test/greeter.test.ts
@@ -0,0 +1,7 @@
+// import { add } from "../src/greeter";
+
+// describe("say hello, world", () => {
+//     it("should return hello world", () => {
+//         expect(consoleLog.toBe("Hello, World"));
+//     })
+// })
